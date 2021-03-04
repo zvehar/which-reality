72eb024c3bbdbe63ddb4c07c25b62b93f82fc655
sec488 <71724119+sec488@users.noreply.github.com>
2021-03-04 12:24:17 -0800

    Update index.php

--- index.php
+++ index.php
@@ -8,60 +8,62 @@
 <body>
 <h1> Server Information</h1>
 <?php
+function command_exist(string $cmd):
+    bool
+    {
+        $return = exec("which " . $cmd);
+        return !empty($return);
+    }
 
-function command_exist(string $cmd): bool
-{
-    $return = exec("which " . $cmd) ;
-    return !empty($return) ;
-}
-
-$OS_NAME = "";
-$OS_VER = "";
-$WEBPROG = "";
-
-if (command_exist("uname")) 
-{
-    $OS_NAME = exec("uname -r") ;
+    $OS_NAME = "";
     $OS_VER = "";
-}
+    $WEBPROG = "";
 
-echo "Kernel version: " . $OS_NAME . " " . $OS_VER . "<br/>";
-
-if (command_exist("apache2")) 
-{
-    $WEBPROG = exec("apache2 -V | grep ^Server\ version") ;
-    if ($WEBPROG == "") 
+    if (command_exist("uname"))
     {
-        $WEBPROG = "Web Server: Apache (Undetermined version)" ;
+        $OS_NAME = exec("uname -r");
+        $OS_VER = "";
     }
-} elseif (command_exist("httpd")) 
-{
-    $WEBPROG = exec("httpd -V | grep ^Server\ version") ;
-    if ($WEBPROG == "") 
-    {
-        $WEBPROG = "Web Server: Apache (Undetermined version)" ;
-    }
-} elseif (command_exist("nginx")) 
-{
-    $WEBPROG = exec("nginx -V | grep ^nginx\ version") ;
-    if ($WEBPROG == "") 
-    {
-        $WEBPROG = "Web Server: NGINX (Undetermined version)";
-    }
-}
 
-echo $WEBPROG . "<br/>";
+    echo "Kernel version: " . $OS_NAME . " " . $OS_VER . "<br/>";
 
-echo "PHP Version: " . phpversion() . "<br/>";
+    if (command_exist("apache2"))
+    {
+        $WEBPROG = exec("apache2 -V | grep ^Server\ version");
+        if ($WEBPROG == "")
+        {
+            $WEBPROG = "Web Server: Apache (Undetermined version)";
+        }
+    }
+    elseif (command_exist("httpd"))
+    {
+        $WEBPROG = exec("httpd -V | grep ^Server\ version");
+        if ($WEBPROG == "")
+        {
+            $WEBPROG = "Web Server: Apache (Undetermined version)";
+        }
+    }
+    elseif (command_exist("nginx"))
+    {
+        $WEBPROG = exec("nginx -V | grep ^nginx\ version");
+        if ($WEBPROG == "")
+        {
+            $WEBPROG = "Web Server: NGINX (Undetermined version)";
+        }
+    }
+
+    echo $WEBPROG . "<br/>";
+
+    echo "PHP Version: " . phpversion() . "<br/>";
 ?>
 
 <h1> Running Processes </h1>
 <?php
-exec("ps -ef", $output);
-foreach ($output as $i) 
-{
-    echo $i . "<br/>";
-}
+    exec("ps -ef", $output);
+    foreach ($output as $i)
+    {
+        echo $i . "<br/>";
+    }
 ?>
 </body>
 </html>
