6ff5216b7193fe3c1cdf7481e0a0f06fffc12cda
zvehar <100161860+zvehar@users.noreply.github.com>
2022-02-22 13:01:37 -0600

    Update index.php
    Removed suspicious comment lab2.2

---
+++ index.php
@@ -0,0 +1,52 @@
+<html>
+<head>
+  <title>Oh geez... What reality am I in?</title>
+</head>
+  
+<body>
+<h1> Server Information</h1>
+<?php 
+
+function command_exist($cmd) {
+    $return = shell_exec(sprintf("which %s", escapeshellarg($cmd)));
+    return !empty($return);
+}
+
+if (command_exist("uname")) {
+    $OS_NAME = exec("uname -r");
+    $OS_VER = "";
+}
+
+echo "Kernel version: " . $OS_NAME . " " . $OS_VER . "<br/>";
+
+if (command_exist("apache2")) {
+    $WEBPROG = exec("apache2 -V | grep ^Server\ version");
+    if ($WEBPROG == "") {
+        $WEBPROG = "Web Server: Apache (Undetermined version)";
+    }
+} elseif (command_exist("httpd")) {
+    $WEBPROG = exec("httpd -V | grep ^Server\ version");
+    if ($WEBPROG == "") {
+        $WEBPROG = "Web Server: Apache (Undetermined version)";
+    }
+} elseif (command_exist("nginx")) {
+    $WEBPROG = exec("nginx -V | grep ^nginx\ version");
+    if ($WEBPROG == "") {
+        $WEBPROG = "Web Server: NGINX (Undetermined version)";
+    }
+}
+
+echo $WEBPROG . "<br/>";
+
+echo "PHP Version: " . phpversion() . "<br/>";
+?>
+
+<h1> Running Processes </h1>
+<?php
+exec("ps -ef", $output);
+foreach($output as $i) {
+    echo $i . "<br/>";
+}
+?>
+</body>
+</html>
